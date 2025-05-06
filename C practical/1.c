e8510f284172a38505b884f133270f8630d0c335
VISHAL MAHENDRA MORE <vishalmoreee123@gmail.com>
2025-05-06 11:19:48 +0530

    Add files via upload

---
+++ C practical/1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+// ex.(a-b)2
+void main(){
+	int a=10; //create variable a give value 2
+	int b=20; //create variable b give value 6
+	//(a-b)2 = (a*a)-2*a*b+(b*b) 
+	printf("(a-b)2=%d\n",(a*a)-2*a*b+(b*b));//
+	
+	
+}
---
+++ C practical/2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	int a=5;
+	int b=8;
+	printf("(a+b)2=%d",(a*a)+2*a*b+(b*b));
+	
+}
