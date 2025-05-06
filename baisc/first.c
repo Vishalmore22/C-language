d6a9f789f364cea1c30b6277f02353e415b000d9
VISHAL MAHENDRA MORE <vishalmoreee123@gmail.com>
2025-05-06 11:06:03 +0530

    Delete baisc directory

--- baisc/first.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-
-void main(){
-	printf("student name : vishal more\ngender:male\ncontact no:6365011737\nemail:vishalmoreee123@gmail.com\ncoures:full stack dev");
-
-}
