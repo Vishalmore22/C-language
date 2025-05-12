ca622a5a1fbcc58c03c127dd6fd4b1f3ec6e5fa1
VISHAL MAHENDRA MORE <vishalmoreee123@gmail.com>
2025-05-12 11:41:07 +0530

    Delete C-pratical/data types directory

--- C-pratical/data types/3 change multiple value.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-//change multiple value
-void main(){
-	int adani=500600;
-	printf("A  adani=%d\n",adani);
-	printf("B  adani=%d\n",adani);
-	printf("C  adani=%d\n",adani);
-}
