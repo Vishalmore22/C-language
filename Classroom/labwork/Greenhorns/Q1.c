d5d15171b847913eb179823731c37e2536eb5e2c
VISHAL MAHENDRA MORE <vishalmoreee123@gmail.com>
2025-05-22 10:11:40 +0530

    Add files via upload

---
+++ Classroom/labwork/Greenhorns/Q1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+//Write C Program to print below design using printf function with help of \t & \n escape sequence
+void main()
+{
+	printf("   *  *\n");
+	printf("  *  *\n");
+	printf("*  *\n");
+	printf("  *  *\n");
+	printf("   *  *\n");
+	
+	printf("\n");
+	
+	
+	
+	
+	printf("   *  *  *  *  *  *  *\n");
+	
+	printf("*  *  *  *  *  *  *  *\n");
+	printf("*     *              *\n");
+	printf("*     *     *  *  *  *\n");
+	printf("*     *     *     *  *\n");
+	printf("*  *  *  *  *  *  *  *\n");
+}
+
