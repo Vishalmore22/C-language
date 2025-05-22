0a69984194e8348db76b3df9072820eeb83026b3
VISHAL MAHENDRA MORE <vishalmoreee123@gmail.com>
2025-05-22 13:58:46 +0530

    Add files via upload

---
+++ Classroom/labwork/Control Structure/L-2/Q3.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	
+	int a,b,c,d;
+	
+	printf("Enter a :");
+	scanf("%d",&a);
+	
+	printf("Enter b :");
+	scanf("%d",&b);
+	
+	printf("Enter c :");
+	scanf("%d",&c);
+	
+	printf("Enter d :");
+	scanf("%d",&d);
+	
+	if(a==b && b==c && c==d)
+	{
+		printf("all are equal");
+	}
+	else
+	{
+		if(a>b)
+		{
+			if(a>c)
+			{
+				if(a>d)
+				{
+					printf("a is max");
+				}
+				else
+				{
+					printf("d is max");
+				}
+			}
+			else
+			{
+				if(c>d)
+				{
+					printf("c is max");
+				}
+				else
+				{
+					printf("d is max");
+				}
+			}
+		}
+		else
+		{
+			if(b>c)
+			{
+				if(b>d)
+				{
+					printf("b is max");
+				}
+				else
+				{
+					printf("d is max");
+				}
+			}
+			else
+			{
+				if(c>d)
+				{
+					printf("c is max");
+				}
+				else
+				{
+					printf("d is max");
+				}
+			}
+		}
+	}
+}
---
+++ Classroom/labwork/Control Structure/L-2/Q5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+    int a, b, c, max;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+     max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+
+    printf("Maximum number is: %d\n", max);
+
+    
+}
+
