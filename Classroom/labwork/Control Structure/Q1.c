cb49c7876ba15affcc19d2732c579fb1a376b04a
VISHAL MAHENDRA MORE <vishalmoreee123@gmail.com>
2025-05-22 11:10:39 +0530

    Add files via upload

---
+++ Classroom/labwork/Control Structure/Q1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+//Find min. no. from given 2 no. using if else
+void main()
+{
+	int a=10,b=26;
+	
+	
+	if(a<b)
+	{
+		printf("a is min ");
+	}
+	else
+	{
+		printf("b is min ");
+	}
+	
+	
+}
