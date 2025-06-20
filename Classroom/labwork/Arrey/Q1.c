284aa5fdca80fb9b019b0e518b349cb00d8b094b
VISHAL MAHENDRA MORE <vishalmoreee123@gmail.com>
2025-06-20 09:59:13 +0530

    Add files via upload

---
+++ Classroom/labwork/Arrey/Q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+//WAP to perform all arithmetic operator such as +, -, *, /, %
+void main()
+{
+	int a=10,b=15;
+	printf("%d + %d =%d\n",a,b,a+b);
+	printf("%d - %d =%d\n",a,b,a-b);
+	printf("%d * %d =%d\n",a,b,a*b);
+	printf("%d / %d =%d\n",a,b,a/b);	
+}
